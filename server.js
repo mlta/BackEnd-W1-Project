6ebf6f66c0ec94544154c661525c316cfd9c6e78
Maria Laura Tarabillo <mlta18@outlook.com>
2017-08-02 18:56:25 -0400

    letters working, spaces working, but everything in the terminal

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
+const fs = require("fs")
 const express = require("express")
 const mustacheExpress = require("mustache-express")
-
 const expressValidator = require("express-validator")
 const bodyParser = require("body-parser")
-
+const lettersthatuserguessed = []
 const app = express()
-
+const words = fs.readFileSync("/usr/share/dict/words", "utf-8").toLowerCase().split("\n")
+//console.log(words)
 app.engine("mustache", mustacheExpress())
 app.set("views", "./views")
 app.set("view engine", "mustache")
@@ -18,9 +19,35 @@ app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use(expressValidator())
 
+const randomWord = words[Math.floor(Math.random() * words.length)]
+console.log(Math.floor(Math.random() * words.length))
+console.log(words[Math.floor(Math.random() * words.length)])
+
 app.get("/", (request, response) => {
-  response.render("home")
+  let conversionUnderscores = []
+  console.log(randomWord)
+  //   for (var i = 0; i < randomWord.length; i++) {
+  //    if(/*checn if the the current letter of the randomWord has been used*/)
+  //     conversionUnderscores.push(" _ ")
+  //   }
+
+  //console.log(conversionUnderscores)
+  let lettersfound = conversionUnderscores.join("")
+  //
+  response.render("home", { lettersfound })
 })
+// only to store it.
+app.post("/", (request, response) => {
+  const storeletter = request.body.letter
+  lettersthatuserguessed.push(storeletter)
+  response.redirect("/")
+})
+
+//app.post("/", (request, response) => {
+//const letterssplit = randomWord.split("")
+//console.log(letterssplit)
+// response.render("home")
+//})
 
 app.listen(3000, () => {
   console.log("Funciona")
